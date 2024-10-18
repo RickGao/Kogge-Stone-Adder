870545f3fc3830cf4e99dc65f2d041829c46d7d7
RickGao <rickgao6@outlook.com>
2024-10-18 14:25:31 -0400

    Kogge Stone Adder

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 RickGao
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_koggestone_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,52 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // wire _unused = &{ena, clk, rst_n, 1'b0};
+
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uio_out = 8'b00000000;
+  assign uio_oe = 8'b00000000;
+
+  wire[3:0] a, b;
+  assign a = ui_in[3:0];
+  assign b = ui_in[7:4];
+  
+  wire[3:0] gen, pro; // Generate, Propagate
+  assign gen = a & b;
+  assign pro = a ^ b;
+
+
+  // Layer 1   i - 1
+  wire gen1_l1, gen2_l1, gen3_l1, pro2_l1, pro3_l1;
+  assign gen1_l1 = gen[1] | (pro[1] & gen[0]);
+  assign gen2_l1 = gen[2] | (pro[2] & gen[1]);
+  assign gen3_l1 = gen[3] | (pro[3] & gen[2]);
+  assign pro2_l1 = pro[2] & pro[1];
+  assign pro3_l1 = pro[3] & pro[2];
+
+  // Layer 2   i - 2
+  wire gen2_l2, gen3_l2;
+  assign gen2_l2 = gen2_l1 | (pro2_l1 & gen[0]);
+  assign gen3_l2 = gen3_l1 | (pro3_l1 & gen1_l1);
+  
+  // Output
+  wire[3:0] carry, sum;
+  wire carry_out;
+  assign carry[0] = 0;
+  assign carry[1] = gen[0];
+  assign carry[2] = gen1_l1;
+  assign carry[3] = gen2_l2;
+  assign carry_out = gen3_l2; 
+
+  assign sum = pro ^ carry;
+  
+  assign uo_out[3:0] = sum;
+  assign uo_out[4] = carry_out;
+
+  assign uo_out[7:5] = 3'b000;
+  assign uio_out = 8'b00000000;
+  assign uio_oe = 8'b00000000;
 
 endmodule
